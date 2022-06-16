3c3808387efd4f3c7d8cbaa5a4f7f2de5e1b4af5
radunica66 <mislav.cotic@gmail.com>
2022-06-16 19:54:13 +0200

    Require user and same user restrictions for articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
 
-
   helper_method :current_user, :logged_in?
 
   def current_user
@@ -10,4 +9,12 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
+
 end
